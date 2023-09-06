a1a8bc3a9eeb54c9ed0b15d133b8846968387fbc
lzy <1204375052@qq.com>
2023-09-06 17:58:14 +0800

    gateway 组件集成

---
+++ gateway/src/main/java/lyzzcw/work/speedkill/gateway/GatewayStarter.java
@@ -0,0 +1,23 @@
+package lyzzcw.work.speedkill.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lzy
+ * @version 1.0
+ * Date: 2023/9/6 13:53
+ * Description: No Description
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+        RedisAutoConfiguration.class})
+public class GatewayStarter {
+    public static void main(String[] args) {
+        System.setProperty("user.home", "/home/lzy/gateway");
+        SpringApplication.run(GatewayStarter.class, args);
+    }
+}
